168c64064637ae48bf68214c336f619ea6d1c3b1
Eduardo Rosas <eduardorosas@MacBook-Pro-de-Eduardo.local>
2018-12-28 11:43:55 -0600

    Initial project setup

---
+++ PortraitLandscapeExample/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PortraitLandscapeExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+
+            PopulateCategories();
+        }
+
+        private void PopulateCategories()
+        {
+            Random rnd = new Random();
+            List<Category> categories = new List<Category>();
+            string[] importanceValues = { "Low priority", "Important", "High priority" };
+            for (int i = 0; i < 30; i++)
+            {
+                int importance = rnd.Next(0, 3);
+                Category newCategory = new Category
+                {
+                    Importance = importanceValues[importance],
+                    Name = $"Category {i}"
+                };
+                categories.Add(newCategory);
+            }
+
+            mainListView.ItemsSource = categories;
+        }
+    }
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public string Importance { get; set; }
+    }
+}
